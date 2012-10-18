7bd2bd930dadc8f4d5df8970a5028a3dbf24f2d6
Pedro Pontes <pedro@exporgal.com>
2012-10-18 18:01:21 +0100

    app bundle

---
+++ icoach/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icoach::Application.config.secret_token = '3ab1a275d4c98b373daa52b65589edb098d2663f6c2b75ae89ef832d8ac2554887372c0c91848dbbfe97dab9d1f88da355b9d3693f435c077847dddced489360'
