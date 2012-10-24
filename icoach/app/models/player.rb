2479340c40aedc0349b093b4a1ed9911785115e6
Joel Ferreira <joelf087@hotmail.com>
2012-10-24 15:04:58 +0100

    Fixed bug when adding a new player

--- icoach/app/models/player.rb
+++ icoach/app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
-  validates :height, :numericality => {greater_than => 0}
-  validates :weight, :numericality => {greater_than => 0}
+  validates :height, :numericality => {:greater_than => 0}
+  validates :weight, :numericality => {:greater_than => 0}
 
   has_and_belongs_to_many :teams
 end
