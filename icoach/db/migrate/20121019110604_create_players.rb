3eeeced2f089363b41cfa6a7f1fb8486410eae2e
Pedro Pontes <pedro@exporgal.com>
2012-10-24 14:26:31 +0100

    Just some css to help visualizing

--- icoach/db/migrate/20121019110604_create_players.rb
+++ icoach/db/migrate/20121019110604_create_players.rb
@@ -13,12 +13,3 @@ class CreatePlayers < ActiveRecord::Migration
     end
   end
 end
-
-#class CreatePlayerTeamJoinTable < ActiveRecord::Migration
-#  def change
-#    create_table :players_teams, :id => false do |t|
-#      t.integer :player_id
-#     t.integer :team_id
-#   end
-#  end
-#end
