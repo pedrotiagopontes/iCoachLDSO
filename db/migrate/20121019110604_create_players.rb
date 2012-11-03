bb4b3b867b6c807524e14b518aade1cb301b7b7f
pedrotiagopontes <pedrotiagopontes@gmail.com>
2012-11-03 00:38:52 +0000

    changing dirs because of heroku

---
+++ db/migrate/20121019110604_create_players.rb
@@ -0,0 +1,15 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.decimal :height
+      t.date :date_of_birth
+      t.string :nationality
+      t.decimal :weight
+      t.string :position
+      t.references :team
+
+      t.timestamps
+    end
+  end
+end
--- icoach/app/models/player.rb
+++
@@ -1,6 +0,0 @@
-class Player < ActiveRecord::Base
-  validates :height, :numericality => {:greater_than => 0}
-  validates :weight, :numericality => {:greater_than => 0}
-
-  has_and_belongs_to_many :teams
-end
