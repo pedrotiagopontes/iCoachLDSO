e1669dba1fa94feafd0065195ce7b8dae9c87d01
pedrotiagopontes <pedrotiagopontes@gmail.com>
2012-11-26 12:06:48 +0000

    tokens controller update

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -5,13 +5,13 @@ class TokensController  < ApplicationController
     email = params[:email]
     password = params[:password]
     if request.format != :json
-      render :status=>406, :json=>{:message=>"The request must be json"}
+      render :status=>406, :json=>{:error=>"The request must be json"}
       return
     end
 
     if email.nil? or password.nil?
      render :status=>400,
-     :json=>{:message=>"The request must contain the user email and password."}
+     :json=>{:error=>"The request must contain the user email and password."}
      return
     end
 
@@ -19,7 +19,7 @@ class TokensController  < ApplicationController
 
     if @user.nil?
       logger.info("User #{email} failed signin, user cannot be found.")
-      render :status=>401, :json=>{:message=>"Invalid email or passoword."}
+      render :status=>401, :json=>{:error=>"Invalid email or passoword."}
       return
     end
 
@@ -27,10 +27,10 @@ class TokensController  < ApplicationController
     @user.ensure_authentication_token!
 
     if not @user.valid_password?(password)
-      logger.info("User #{email} failed signin, password \"#{password}\" is invalid")
-      render :status=>401, :json=>{:message=>"Invalid email or password."}
+      logger.info("User #{email} failed signin, password is invalid")
+      render :status=>401, :json=>{:error=>"Invalid email or password."}
     else
-      render :status=>200, :json=>{:token=>@user.authentication_token}
+      render :status=>200, :json=>{:token=>@user.authentication_token, :id => @user.id}
     end
   end
 
@@ -38,7 +38,7 @@ class TokensController  < ApplicationController
     @user=User.find_by_authentication_token(params[:id])
     if @user.nil?
       logger.info("Token not found.")
-      render :status=>404, :json=>{:message=>"Invalid token."}
+      render :status=>404, :json=>{:error=>"Invalid token."}
     else
       @user.reset_authentication_token!
       render :status=>200, :json=>{:token=>params[:id]}
