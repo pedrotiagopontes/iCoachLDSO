7ff4e8bb3e409ea0decff7e1450228eee5901cfa
pedrotiagopontes <pedrotiagopontes@gmail.com>
2012-12-09 23:31:59 +0000

    fixing bug on game show

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,5 @@
 class Game < ActiveRecord::Base
   belongs_to :team
-  #has_many :playersgames
-  #has_many :players, :through => :playersgames
   has_many :events
   has_many :convocations
   has_many :players, :through => :convocations
