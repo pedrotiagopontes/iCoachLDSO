cd751bb3b4bd2330151dd5cbed945107dddabfb8
Joao Henriques <joaoh88@gmail.com>
2012-12-29 19:49:25 +0000

    Fix notes, crashing rails when submited null values)

--- db/migrate/20121209233022_create_notes.rb
+++ db/migrate/20121209233022_create_notes.rb
@@ -1,8 +1,8 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
-      t.string :title
-      t.text :text
+      t.string :title, :null => false
+      t.text :text, :null => false
       t.references :user
 
       t.timestamps
